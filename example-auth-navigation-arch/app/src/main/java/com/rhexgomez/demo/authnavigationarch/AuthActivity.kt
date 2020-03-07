7c49e86376ecdbf5d26868fe5f9d30b8fd060d55
Elmar Rhex Gomez <12870294+rhexgomez@users.noreply.github.com>
2020-03-07 17:59:04 +0800

    Added toolbar

---
+++ example-auth-navigation-arch/app/src/main/java/com/rhexgomez/demo/authnavigationarch/AuthActivity.kt
@@ -0,0 +1,40 @@
+package com.rhexgomez.demo.authnavigationarch
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_auth.*
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        toolbar.run {
+            setTitleTextAppearance(this@AuthActivity, R.style.ToolbarTextAppearance)
+            setSubtitleTextAppearance(this@AuthActivity, R.style.ToolbarSubtitleTextAppearance)
+            setSupportActionBar(this)
+        }
+
+        supportActionBar?.apply {
+            setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_ios_24)
+            setDisplayHomeAsUpEnabled(true)
+            title = "EXAMPLE"
+            subtitle = "Authentication Flow"
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            android.R.id.home -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
