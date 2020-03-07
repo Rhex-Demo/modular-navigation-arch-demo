66faca540e1b1cf4673c6d9aab063a801e511f37
Elmar Rhex Gomez <12870294+rhexgomez@users.noreply.github.com>
2020-03-07 21:08:02 +0800

    Added registration

--- example-auth-navigation-arch/app/src/main/java/com/rhexgomez/demo/authnavigationarch/RegistrationFragment.kt
+++ example-auth-navigation-arch/app/src/main/java/com/rhexgomez/demo/authnavigationarch/RegistrationFragment.kt
@@ -6,13 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_log_in.*
 
-class LogInFragment : Fragment(R.layout.fragment_log_in) {
+class RegistrationFragment : Fragment(R.layout.fragment_register) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        register.setOnClickListener {
-            findNavController().navigate(LogInFragmentDirections.actionOpenRegistration())
-        }
+
     }
+
 }
